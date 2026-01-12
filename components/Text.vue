2dc25d2400ccbb4a3a48995359cbf947c27b1899
tonimatasdev <79017964+TonimatasDEV@users.noreply.github.com>
2026-01-12 21:18:58 +0100

    chore: I am a self-taught developer

--- components/Text.vue
+++ components/Text.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div class="flex justify-center">
-    <p>I am a backend developer. You can visit my
+    <p>I am a self-taught backend developer. You can visit my
       <a href="https://github.com/TonimatasDEV" target="_blank" class="text-blue-500 underline font-bold">GitHub</a>
       or contact me via email at me@tonimatas.dev.
     </p>
